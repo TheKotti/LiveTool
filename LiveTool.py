1da84fca32a31394a0df94b8ee6ac201a973b8a5
Konsta Mustonen <K8721@student.jamk.fi>
2019-12-06 21:56:25 +0200

    Automatically refresh Twitch OAuth token

--- LiveTool.py
+++ LiveTool.py
@@ -22,23 +22,42 @@ stream_url = data['url']
 """TWITCH"""
 if data['twitch']:
     http = urllib3.PoolManager()
-    encoded_args = urlencode({
+
+    """Refresh OAuth token"""
+    refresh_args = urlencode({
+        'client_id': config['TWITCH']['client_id'],
+        'client_secret': config['TWITCH']['client_secret'],
+        'grant_type': 'refresh_token',
+        'refresh_token': config['TWITCH']['refresh_token']
+    })
+    refresh_url = 'https://id.twitch.tv/oauth2/token?' + refresh_args
+    refresh_request = http.request(
+        'POST',
+        refresh_url
+    )
+    new_token = json.loads(refresh_request.data)['access_token']
+    config.set('TWITCH', 'oauth_token', 'OAuth ' + new_token)
+    with open(os.path.join(location, "config.ini"), 'w') as cnf_file:
+        config.write(cnf_file)
+
+    """Set new stream info"""
+    twitch_args = urlencode({
         'channel[status]': title,
         'channel[game]': game
     })
-    url = 'https://api.twitch.tv/kraken/channels/' + \
-        config['TWITCH']['user_id'] + '?' + encoded_args
+    twitch_udpate_url = 'https://api.twitch.tv/kraken/channels/' + \
+        config['TWITCH']['user_id'] + '?' + twitch_args
 
-    r = http.request(
+    twitch_request = http.request(
         'PUT',
-        url,
+        twitch_udpate_url,
         headers={
             'Client-ID': config['TWITCH']['client_id'],
             'Accept': 'application/vnd.twitchtv.v5+json',
             'Authorization': config['TWITCH']['oauth_token']
         }
     )
-    print(json.loads(r.data.decode('utf-8')))
+    print(json.loads(twitch_request.data.decode('utf-8')))
 
 
 """DISCORD"""
