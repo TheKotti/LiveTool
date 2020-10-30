66051d8a421c4ebc09cc6a1b493aede9c3148479
Konsta Mustonen <konsta.mustonen@gmail.com>
2020-10-30 21:36:01 +0200

    Update IGDB request, add error handling

--- LiveTool.py
+++ LiveTool.py
@@ -33,46 +33,56 @@ stream_url = ' | '.join(streams)
 
 """IGDB"""
 if data['igdb']:
-    http = urllib3.PoolManager()
-    igdb_request = http.request(
-        'GET',
-        'https://api-v3.igdb.com/games',
-        body='search "' + game + '"; fields name,category;',
-        headers={'Content-Type': 'application/json', 'user-key': config['IGDB']['user_key']})
-    games_data = json.loads(igdb_request.data)
-    games_list = []
-    for elem in games_data:
-        if elem['category'] == 0:
-            games_list.append(elem['name'].upper())
-    if game.upper() not in games_list and len(games_list) > 0:
-        best_matches = difflib.get_close_matches(game, games_list, 3, 0)
-        game = best_matches[0]
-    print('Game title fetched (' + game + ')')
+    try:
+        http = urllib3.PoolManager()
+        igdb_request = http.request(
+            'POST',
+            'https://api.igdb.com/v4/games',
+            body='search "' + game + '"; fields name,category;',
+            headers={
+                'Content-Type': 'application/json',
+                'Authorization': config['IGDB']['token'],
+                'Client-ID': config['TWITCH']['client_id']})
+        games_data = json.loads(igdb_request.data)
+        games_list = []
+        for elem in games_data:
+            if elem['category'] == 0:
+                games_list.append(elem['name'].upper())
+        if game.upper() not in games_list and len(games_list) > 0:
+            best_matches = difflib.get_close_matches(game, games_list, 3, 0)
+            game = best_matches[0]
+        print('Game title fetched (' + game + ')')
+    except:
+        print("IGDB ERROR")
 
 
 """TWITCH"""
 if data['twitch']:
-    http = urllib3.PoolManager()
+    try:
+        http = urllib3.PoolManager()
 
-    """ Refresh OAuth token """
-    if config['TWITCH']['refresh_token'] != 'refresh_token':
-        refresh_args = urlencode({
-            'client_id': config['TWITCH']['client_id'],
-            'client_secret': config['TWITCH']['client_secret'],
-            'grant_type': 'refresh_token',
-            'refresh_token': config['TWITCH']['refresh_token']
-        })
-        refresh_url = 'https://id.twitch.tv/oauth2/token?' + refresh_args
-        refresh_request = http.request(
-            'POST',
-            refresh_url
-        )
-        new_token = json.loads(refresh_request.data)['access_token']
-        new_refresh_token = json.loads(refresh_request.data)['refresh_token']
-        config.set('TWITCH', 'oauth_token', 'OAuth ' + new_token)
-        config.set('TWITCH', 'refresh_token', new_refresh_token)
-        with open(os.path.join(location, "config.ini"), 'w') as cnf_file:
-            config.write(cnf_file)
+        """ Refresh OAuth token """
+        if config['TWITCH']['refresh_token'] != 'refresh_token':
+            refresh_args = urlencode({
+                'client_id': config['TWITCH']['client_id'],
+                'client_secret': config['TWITCH']['client_secret'],
+                'grant_type': 'refresh_token',
+                'refresh_token': config['TWITCH']['refresh_token']
+            })
+            refresh_url = 'https://id.twitch.tv/oauth2/token?' + refresh_args
+            refresh_request = http.request(
+                'POST',
+                refresh_url
+            )
+            new_token = json.loads(refresh_request.data)['access_token']
+            new_refresh_token = json.loads(refresh_request.data)[
+                'refresh_token']
+            config.set('TWITCH', 'oauth_token', 'OAuth ' + new_token)
+            config.set('TWITCH', 'refresh_token', new_refresh_token)
+            with open(os.path.join(location, "config.ini"), 'w') as cnf_file:
+                config.write(cnf_file)
+    except:
+        print("TWITCH ERROR")
 
     """ Set new stream info """
     title_with_suffix = title + ' | ' + suffix if suffix != '' else title
@@ -96,68 +106,77 @@ if data['twitch']:
 
 """YOUTUBE"""
 if data['youtube']:
-    """If no refresh token is set, create one"""
-    if config['YOUTUBE']['refresh_token'] == 'refresh_token':
-        yt_scopes = ['https://www.googleapis.com/auth/youtube']
-        yt_secrets = 'youtube_secret.json'
-        flow = InstalledAppFlow.from_client_secrets_file(
-            yt_secrets, yt_scopes)
-        credentials = flow.run_console()
-        config.set('YOUTUBE', 'refresh_token', credentials.refresh_token)
-        with open(os.path.join(location, "config.ini"), 'w') as cnf_file:
-            config.write(cnf_file)
+    try:
+        """If no refresh token is set, create one"""
+        if config['YOUTUBE']['refresh_token'] == 'refresh_token':
+            yt_scopes = ['https://www.googleapis.com/auth/youtube']
+            yt_secrets = 'youtube_secret.json'
+            flow = InstalledAppFlow.from_client_secrets_file(
+                yt_secrets, yt_scopes)
+            credentials = flow.run_console()
+            config.set('YOUTUBE', 'refresh_token', credentials.refresh_token)
+            with open(os.path.join(location, "config.ini"), 'w') as cnf_file:
+                config.write(cnf_file)
 
-    yt_credentials = Credentials(
-        None,
-        refresh_token=config['YOUTUBE']['refresh_token'],
-        token_uri="https://accounts.google.com/o/oauth2/token",
-        client_id=config['YOUTUBE']['client_id'],
-        client_secret=config['YOUTUBE']['client_secret'],
-    )
-
-    youtube = build('youtube', 'v3', credentials=yt_credentials)
-    timeNow = datetime.datetime.now().isoformat()[:-6] + '000Z'
-
-    list_broadcasts_request = youtube.liveBroadcasts().list(
-        part='id,snippet',
-        maxResults=1,
-        mine=True,
-        broadcastType='all'
-    )
-
-    list_broadcasts_response = list_broadcasts_request.execute()
-    broadcast_snippet = list_broadcasts_response['items'][0]['snippet']
-    broadcast_snippet['title'] = title
-
-    update_broadcast_request = youtube.liveBroadcasts().update(
-        part='id,snippet',
-        body=dict(
-            snippet=broadcast_snippet,
-            id=list_broadcasts_response['items'][0]['id']
+        yt_credentials = Credentials(
+            None,
+            refresh_token=config['YOUTUBE']['refresh_token'],
+            token_uri="https://accounts.google.com/o/oauth2/token",
+            client_id=config['YOUTUBE']['client_id'],
+            client_secret=config['YOUTUBE']['client_secret'],
         )
-    ).execute()
-    print('Youtube info changed')
+
+        youtube = build('youtube', 'v3', credentials=yt_credentials)
+        timeNow = datetime.datetime.now().isoformat()[:-6] + '000Z'
+
+        list_broadcasts_request = youtube.liveBroadcasts().list(
+            part='id,snippet',
+            maxResults=1,
+            mine=True,
+            broadcastType='all'
+        )
+
+        list_broadcasts_response = list_broadcasts_request.execute()
+        broadcast_snippet = list_broadcasts_response['items'][0]['snippet']
+        broadcast_snippet['title'] = title
+
+        update_broadcast_request = youtube.liveBroadcasts().update(
+            part='id,snippet',
+            body=dict(
+                snippet=broadcast_snippet,
+                id=list_broadcasts_response['items'][0]['id']
+            )
+        ).execute()
+        print('Youtube info changed')
+    except:
+        print("YOUTUBE ERROR")
 
 
 """DISCORD"""
 if data['discord']:
-    discord_message = title + ": " + stream_url
-    webhook = DiscordWebhook(
-        url=config['DISCORD']['webhook_url'], content=discord_message)
-    webhook.execute()
-    print('Discord message posted')
+    try:
+        discord_message = title + ": " + stream_url
+        webhook = DiscordWebhook(
+            url=config['DISCORD']['webhook_url'], content=discord_message)
+        webhook.execute()
+        print('Discord message posted')
+    except:
+        print("DISCORD ERROR")
 
 
 """TWITTER"""
 if data['twitter']:
-    tweet = title + "\n\n" + stream_url
-    twitterApi = twitter.Api(consumer_key=config['TWITTER']['consumer_key'],
-                             consumer_secret=config['TWITTER']['consumer_secret'],
-                             access_token_key=config['TWITTER']['access_token_key'],
-                             access_token_secret=config['TWITTER']['access_token_secret'])
+    try:
+        tweet = title + "\n\n" + stream_url
+        twitterApi = twitter.Api(consumer_key=config['TWITTER']['consumer_key'],
+                                 consumer_secret=config['TWITTER']['consumer_secret'],
+                                 access_token_key=config['TWITTER']['access_token_key'],
+                                 access_token_secret=config['TWITTER']['access_token_secret'])
 
-    twitterApi.PostUpdate(tweet)
-    print('Tweet posted')
+        twitterApi.PostUpdate(tweet)
+        print('Tweet posted')
+    except:
+        print("TWITTER ERROR")
 
 print('Exiting...')
 time.sleep(3)
